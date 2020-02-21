aa5c1e9bbc07d322f6fb91625621f489355ef5b8
Petko Yovchevski <pyovchevski@gmail.com>
2020-02-21 10:09:03 +0200

    updates

--- footer.php
+++ footer.php
@@ -3,25 +3,25 @@
         <div class="container">
             <div class="edit nodrop safe-mode" field="new-world_footer" rel="global">
                 <div class="row">
-                    <div class="col-12 col-lg-12 col-xl-3 mx-auto logo-column text-center text-xl-left m-b-20">
+                    <div class="col-12 col-lg-12 col-xl-3 mx-auto logo-column text-center text-xl-left m-b-20 allow-drop">
                         <img src="<?php print template_url(); ?>assets/img/logo_footer.png" alt="" class="m-b-10"/>
                         <br/>
                         <br/>
                     </div>
 
-                    <div class="col-12 col-lg-12 col-xl-5 mx-auto text-center text-xl-left m-b-40">
+                    <div class="col-12 col-lg-12 col-xl-5 mx-auto text-center text-xl-left m-b-40 allow-drop">
                         <module type="menu" template="simple" id="footer_menu" name="footer_menu_1"/>
                         <br/>
                         <p><?php print _lang('Microweber is free open source drag and dop websitre builder and CMS. It is under MIT licenbse and we use Laravel  PHP framework', 'templates/new-world'); ?></p>
                     </div>
 
-                    <div class="col-12 col-sm col-lg-6 col-xl mx-auto text-white">
+                    <div class="col-12 col-sm col-lg-6 col-xl mx-auto text-white allow-drop">
                         <h6 class="m-b-10 m-t-5">Subscribe for Newsletter</h6>
                         <module type="contact_form" template="footer" id="footer_newsletter"/>
                         <?php /* <module type="newsletter" id="footer_newsletter"/> */ ?>
                     </div>
 
-                    <div class="col-12 col-sm col-lg-6 col-xl mx-auto text-white text-center text-sm-right">
+                    <div class="col-12 col-sm col-lg-6 col-xl mx-auto text-white text-center text-sm-right allow-drop">
                         <h6 class="m-t-5">Social Networks</h6>
 
                         <module type="social_links" id="footer_socials"/>

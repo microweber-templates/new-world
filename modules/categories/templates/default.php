443efc38d22f9031b8a48d73dd9046eceddc6201
Petko Yovchevski <pyovchevski@gmail.com>
2020-02-24 15:39:46 +0200

    updates

--- modules/categories/templates/default.php
+++ modules/categories/templates/default.php
@@ -13,14 +13,10 @@ description: List Navigation
 ?>
 
 <?php
-$params['ul_class'] = 'nav nav-list';
+$params['ul_class'] = 'mw-cats-menu';
 $params['ul_class_deep'] = 'nav nav-list';
-$params['link'] = '<a itemprop="url" data-item-id="{id}" class="menu_element_link {nest_level}"  href="{link}"><span class="dot"></span><span>{title}</span></a>';
-
-
 ?>
 
-<div class="module-categories module-categories-template-default">
+<div class="module-categories module-categories-template-horizontal-list-1">
     <?php category_tree($params); ?>
 </div>
-

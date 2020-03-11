d15478fbb21aba5933f0909d139c1d401b04bc2b
Petko Yovchevski <pyovchevski@gmail.com>
2020-03-11 16:43:09 +0200

    updates

--- layouts/blog_inner.php
+++ layouts/blog_inner.php
@@ -1,22 +1,22 @@
 <?php include template_dir() . "header.php"; ?>
-    <div class="main-content" id="blog-content-<?php print CONTENT_ID; ?>">
-        <?php
-        $post = get_content_by_id(CONTENT_ID);
-        $picture = get_picture(CONTENT_ID);
+<div class="main-content" id="blog-content-<?php print CONTENT_ID; ?>">
+<?php
+$post = get_content_by_id(CONTENT_ID);
+$picture = get_picture(CONTENT_ID);
 
-        if (!$picture) {
-            $picture = '';
-        }
+if (!$picture) {
+    $picture = '';
+}
 
-        $itemData = content_data($post['id']);
-        $itemTags = content_tags($post['id']);
-        ?>
+$itemData = content_data($post['id']);
+$itemTags = content_tags($post['id']);
+?>
 
-        <?php if ($picture != '' AND $picture != false): ?>
-            <section class="section-19">
-                <div class="container">
-                    <div class="background-image-holder d-flex" style="background-image: url('<?php print $picture; ?>');">
-                        <?php /*<div class="row w-100 m-0 align-self-center">
+<?php if ($picture != '' AND $picture != false): ?>
+    <section class="section-19">
+        <div class="container">
+            <div class="background-image-holder d-flex" style="background-image: url('<?php print $picture; ?>');">
+                <?php /*<div class="row w-100 m-0 align-self-center">
                             <div class="col-lg-12">
                                 <div class="d-flex w-100 h-100">
                                     <div class="w-100 align-self-center info-holder text-white">
@@ -25,11 +25,11 @@
                                     </div>
                                 </div>
                             </div>*/ ?>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        <?php else: ?>
+            </div>
+        </div>
+    </div>
+    </section>
+<?php else: ?>
     <?php /*
             <section class="section-19">
                 <div class="container">
@@ -47,64 +47,64 @@
                     </div>
                 </div>
             </section>*/ ?>
-        <?php endif; ?>
+<?php endif; ?>
 
-        <section class="section">
-            <div class="container">
-                <div class="text-holder text-center">
-                    <h1 class="m-b-30 text-dark"><?php echo $post['title']; ?></h1>
-                </div>
+    <section class="section">
+        <div class="container">
+            <div class="text-holder text-center">
+                <h1 class="m-b-30 text-dark"><?php echo $post['title']; ?></h1>
             </div>
-        </section>
+        </div>
+    </section>
 
-        <div class="blog-inner-page" id="blog-content-<?php print CONTENT_ID; ?>">
-            <div class="container m-t-30 m-b-50">
-                <div class="row">
-                    <div class="col-xl-6 mx-auto">
-                        <div class="description edit dropcap typography-area" field="content" rel="content">
-                            <?php
-                            //include(template_dir() . 'elements' . DS . 'paragraph-highlight.php');
-                            //include(template_dir() . 'elements' . DS . 'paragraph-lead.php');
-                            include(template_dir() . 'elements' . DS . 'paragraph.php');
-                            //include(template_dir() . 'elements' . DS . 'titles' . DS . 'title-2.php');
-                            //include(template_dir() . 'elements' . DS . 'paragraph.php');
-                            //include(template_dir() . 'elements' . DS . 'blockquote.php');
-                            //include(template_dir() . 'elements' . DS . 'titles' . DS . 'title-3.php');
-                            //include(template_dir() . 'elements' . DS . 'paragraph.php');
-                           // include(template_dir() . 'elements' . DS . 'ordered-list.php');
-                            //include(template_dir() . 'elements' . DS . 'paragraph.php');
-                            //include(template_dir() . 'elements' . DS . 'unordered-list.php');
-                            ?>
-                        </div>
+    <div class="blog-inner-page" id="blog-content-<?php print CONTENT_ID; ?>">
+        <div class="container m-t-30 m-b-50">
+            <div class="row">
+                <div class="col-xl-6 mx-auto">
+                    <div class="description edit dropcap typography-area" field="content" rel="content">
+                        <?php
+                        //include(template_dir() . 'elements' . DS . 'paragraph-highlight.php');
+                        //include(template_dir() . 'elements' . DS . 'paragraph-lead.php');
+                        include(template_dir() . 'elements' . DS . 'paragraph.php');
+                        //include(template_dir() . 'elements' . DS . 'titles' . DS . 'title-2.php');
+                        //include(template_dir() . 'elements' . DS . 'paragraph.php');
+                        //include(template_dir() . 'elements' . DS . 'blockquote.php');
+                        //include(template_dir() . 'elements' . DS . 'titles' . DS . 'title-3.php');
+                        //include(template_dir() . 'elements' . DS . 'paragraph.php');
+                        // include(template_dir() . 'elements' . DS . 'ordered-list.php');
+                        //include(template_dir() . 'elements' . DS . 'paragraph.php');
+                        //include(template_dir() . 'elements' . DS . 'unordered-list.php');
+                        ?>
+                    </div>
 
-                        <div class="text-right m-b-20">
-                            <h6 class="text-dark"><?php echo date('d M Y', strtotime($post['updated_at'])); ?></h6>
-                        </div>
+                    <div class="text-right m-b-20">
+                        <h6 class="text-dark"><?php echo date('d M Y', strtotime($post['updated_at'])); ?></h6>
+                    </div>
 
-                        <div class="border-top-bottom">
-                            <div class="m-t-40 m-b-40 row">
-                                <div class="col-sm-4">
-                                    <?php if ($itemTags): ?>
-                                        <div class="posts-tags">
-                                            <?php foreach ($itemTags as $tag): ?>
-                                                <span class="badge badge-primary"><?php echo $tag; ?></span>
-                                            <?php endforeach; ?>
-                                        </div>
-                                    <?php endif; ?>
-                                </div>
-                                <div class="col-sm-8">
-                                    <module type="sharer" id="post-bottom-sharer" template="skin-1" class="float-right" style="xwidth: calc(100% - 45px);"/>
-                                    <div class="float-right">Share</div>
-                                </div>
+                    <div class="border-top-bottom">
+                        <div class="m-t-40 m-b-40 row">
+                            <div class="col-sm-4">
+                                <?php if ($itemTags): ?>
+                                    <div class="posts-tags">
+                                        <?php foreach ($itemTags as $tag): ?>
+                                            <a href="<?php print content_link(PAGE_ID); ?>/tags:<?php print url_title($tag) ?>"><span class="badge badge-primary"><?php echo $tag; ?></span></a>
+                                        <?php endforeach; ?>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                            <div class="col-sm-8">
+                                <module type="sharer" id="post-bottom-sharer" template="skin-1" class="float-right" style="xwidth: calc(100% - 45px);"/>
+                                <div class="float-right">Share</div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="edit safe-mode nodrop blog-inner-page" field="blog-inner" rel="page">
-            <module type="comments" template="default"/>
-        </div>
+    <div class="edit safe-mode nodrop blog-inner-page" field="blog-inner" rel="page">
+        <module type="comments" template="default"/>
+    </div>
     </div>
 <?php include template_dir() . "footer.php"; ?>

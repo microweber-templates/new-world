30b926e965ca99df8fa27a67f83262b6f0ded316
Front-End Dev <dev@microweber.com>
2021-02-12 10:15:11 +0200

    Added labels on registration form

--- modules/users/profile/templates/default.php
+++ modules/users/profile/templates/default.php
@@ -1,56 +1,59 @@
 <?php $user = get_user_by_id(user_id()); ?>
-<div>
-    <script>
-        saveuserdata = function () {
-            var data = mw.serializeFields('#user-data');
-            if (data.password != data.password2) {
-                mw.$('#errnotification').html('Passwords do not match').show();
-                return false;
-            } else {
-                mw.$('#errnotification').hide();
+<script>
+    saveuserdata = function () {
+        var data = mw.serializeFields('#user-data');
+        if (data.password != data.password2) {
+            mw.$('#errnotification').html('Passwords do not match').show();
+            return false;
+        } else {
+            mw.$('#errnotification').hide();
 
-                if (data.password == '') {
-                    delete data.password;
-                    delete data.password2;
-                }
+            if (data.password == '') {
+                delete data.password;
+                delete data.password2;
             }
-            mw.tools.loading('#user-data')
-            $.post("<?php print api_url(); ?>save_user", data, function () {
-                mw.tools.loading('#user-data', false);
-            });
         }
-    </script>
+        mw.tools.loading('#user-data')
+        $.post("<?php print api_url(); ?>save_user", data, function () {
+            mw.tools.loading('#user-data', false);
+        });
+    }
+</script>
 
-
-    <p>From this window you can edit your profile.<br/><br/></p>
+    <p><?php _lang('From this window you can edit your profile.', "templates/new-world"); ?><br/><br/></p>
 
     <form method="post" id="user-data">
         <div class="mw-ui-box mw-ui-box-important mw-ui-box-content" id="errnotification" style="display: none;margin-bottom: 12px;"></div>
 
         <div class="form-group">
-            <input class="form-control input-lg" type="text" name="username" value="<?php print $user['username']; ?>" placeholder=""<?php _lang('Username or E-mail', "templates/new-world"); ?>">
+            <label class="control-label"><?php _lang("Username", "templates/new-world"); ?></label>
+            <input class="form-control input-lg" type="text" name="username" value="<?php print $user['username']; ?>" placeholder="<?php _lang('Username', "templates/new-world"); ?>">
         </div>
 
         <div class="form-group">
-            <input class="form-control input-lg" type="email" name="email" value="<?php print $user['email']; ?>" placeholder=""<?php _lang('E-mail', "templates/new-world"); ?>">
+            <label class="control-label"><?php _lang("E-mail", "templates/new-world"); ?></label>
+            <input class="form-control input-lg" type="email" name="email" value="<?php print $user['email']; ?>" placeholder="<?php _lang('E-mail', "templates/new-world"); ?>">
         </div>
 
         <div class="form-group">
+            <label class="control-label"><?php _lang("First name", "templates/new-world"); ?></label>
             <input class="form-control input-lg" type="text" name="first_name" value="<?php print $user['first_name']; ?>" placeholder="<?php _lang('First name', "templates/new-world"); ?>">
         </div>
 
         <div class="form-group">
+            <label class="control-label"><?php _lang("Last name", "templates/new-world"); ?></label>
             <input class="form-control input-lg" type="text" name="last_name" value="<?php print $user['last_name']; ?>" placeholder="<?php _lang('Last name', "templates/new-world"); ?>">
         </div>
 
         <div class="form-group">
+            <label class="control-label"><?php _lang("New Password", "templates/new-world"); ?></label>
             <input class="form-control input-lg" type="password" name="password" placeholder="<?php _lang('New Password', "templates/new-world"); ?>">
         </div>
 
         <div class="form-group">
+            <label class="control-label"><?php _lang("Confirm Password", "templates/new-world"); ?></label>
             <input class="form-control input-lg" type="password" name="password2" placeholder="<?php _lang('Confirm Password', "templates/new-world"); ?>">
         </div>
 
         <button type="button" class="btn btn-default btn-lg btn-block m-t-10" onclick="saveuserdata()"><?php _lang('Save', "templates/new-world"); ?></button>
     </form>
-</div>

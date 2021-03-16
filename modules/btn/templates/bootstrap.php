ba70716cedabe0f889879976781707fef09cc3a9
raikov <raikov@ymail.com>
2021-03-16 16:54:29 +0200

    button

--- modules/btn/templates/bootstrap.php
+++ modules/btn/templates/bootstrap.php
@@ -12,14 +12,9 @@ description: Default
 ?>
 
 <?php if($action == 'submit'): ?>
-<button type="submit" id="<?php print $btn_id ?>" class="btn <?php print $style . ' ' . $size; ?>">
-    <?php print $text; ?>
-</button>
+<button type="submit" id="<?php print $btn_id ?>" class="btn <?php print $style . ' ' . $size; ?>"><?php print $text; ?></button>
 <?php else: ?>
 <a id="<?php print $btn_id ?>" href="<?php print $url; ?>" <?php if ($blank) {
     print ' target="_blank" ';
-} ?> class="btn <?php print $style . ' ' . $size; ?>">
-
-    <?php print $text; ?>
-</a>
+} ?> class="btn <?php print $style . ' ' . $size; ?>"><?php print $text; ?></a>
 <?php endif; ?>

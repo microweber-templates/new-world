341884175eca6d87c38c3d1e800bd20a33debc1c
raikov <raikov@ymail.com>
2021-03-16 15:00:06 +0200

    video bg

--- modules/layouts/templates/home-banners/skin-3.php
+++ modules/layouts/templates/home-banners/skin-3.php
@@ -41,27 +41,21 @@ if ($vimeo_url) {
 
             $('.js-video', '#<?php echo $params['id']; ?>').attr('src', newurl);
             $('.info-holder', '#<?php echo $params['id']; ?>').hide('slow');
+
         });
     });
 </script>
 <section class="section-2 js-header-transparent <?php print $layout_classes; ?>">
     <div class="background-image-holder d-flex w-100">
         <div class="vimeo-bg-wrapper">
-            <iframe class="js-video" src="https://player.vimeo.com/video/<?php echo $vimeo_url; ?>?background=1&autoplay=0&loop=1&byline=0&title=0" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+            <module type="video" autoplay="true" url="https://vimeo.com/<?php print $vimeo_url; ?>"/>
         </div>
 
         <div class="w-100 align-self-center text-center info-holder allow-drop  edit safe-mode" field="layout-home-banners-skin-3-<?php print $params['id'] ?>" rel="module">
             <h4>An amazing video</h4>
             <h1>shooted in Bulgaria</h1>
-            <br/>
-            <br/>
-            <div class="element d-inline-block m-3 cloneable">
-                <button class="js-play-video btn play-btn"><i class="fas fa-play"></i></button>
-            </div>
+
         </div>
 
-        <div class="socials-holder">
-            <module type="social_links" template="skin-1"/>
-        </div>
     </div>
 </section>

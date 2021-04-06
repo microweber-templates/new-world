1d9d7ef791d459c99f4fc13f7267deeba95b9326
Nikolay <rachkuw@gmail.com>
2021-04-06 16:45:36 +0300

    New 404 page

--- modules/layouts/templates/skin-5.php
+++ modules/layouts/templates/skin-5.php
@@ -27,33 +27,8 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
 <section class="section-18 <?php print $layout_classes; ?> edit safe-mode nodrop" field="layout-skin-5-<?php print $params['id'] ?>" rel="module">
     <div class="container text-white">
         <div class="row">
-            <div class="col-xl-10 mx-auto">
-                <div class="row m-0">
-                    <div class="col-12 col-md-6 bg-primary text-left cloneable">
-                        <div class="allow-drop padding-holder d-flex flex-column">
-                            <h2>Meet us on the following events</h2>
-                            <br/>
-                            <p class="lead"><strong>Contact us today to book your appintment with our representatives.</strong></p>
-
-                            <div class="element button-holder mt-auto">
-                                <module type="btn" template="bootstrap" button_style="btn-outline-primary" button_size="btn-lg" template="bootstrap" button_text="SET A MEETING"/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-12 col-md-6 text-center allow-drop cloneable background-image-holder" style="background-image: url('<?php print template_url(); ?>assets/img/meeting.png');">
-                        <div class="allow-drop padding-holder d-flex flex-column justify-content-center align-items-center">
-                            <h2>18-20 SEP</h2>
-                            <br/>
-                            <p class="lead"><strong>Webit.Festival 2020 Valencia, Spain</strong></p>
-                            <br/>
-                            <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
-                            <div class="element button-holder">
-                                <module type="btn" template="bootstrap" button_style="btn-outline-primary" button_size="btn-lg" button_text="CONTACT US"/>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div class="col-12 text-center">
+                <img src="<?php print template_url(); ?>assets/img/404_graphic.png"/>
             </div>
         </div>
     </div>

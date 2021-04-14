592b5d952190ee89cc3657da64753e96179e046f
Nikolay <rachkuw@gmail.com>
2021-04-14 11:07:08 +0300

    update

--- modules/layouts/templates/galleries/skin-2.php
+++ modules/layouts/templates/galleries/skin-2.php
@@ -27,7 +27,7 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
     <div class="d-flex w-100">
         <div class="container align-self-centerx">
             <div class="row">
-                <div class="col-12 col-xl-12 mx-auto text-center text-white">
+                <div class="col-12 col-xl-12 mx-auto text-center">
                     <h2 class="hr">Explore the features</h2>
 
                     <module type="slider" template="bxslider-skin-2" />

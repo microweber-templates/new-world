9e55f0dd0a97b1a5cc79d35904a86ab00f4aa16e
Peter Ivanov <peter@microweber.com>
2021-04-15 17:05:24 +0300

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 $config = array();
 $config['name'] = "New World";
 $config['author'] = "Microweber";
-$config['version'] = 2.1;
+$config['version'] = 2.2;
 $config['url'] = "http://microweber.com";
 $config['standalone_module_skins'] = true;
 $config['framework'] = "bootstrap4";

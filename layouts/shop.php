a24787e21b4a618e2cbd357221c0a5c1615c6d66
raikov <raikov@ymail.com>
2021-04-19 20:42:22 +0300

    shop layout fixes

--- layouts/shop.php
+++ layouts/shop.php
@@ -19,25 +19,37 @@ if (!isset($shop_sidebar)) {
 }
 ?>
 
-<div class="edit" rel="content" field="new-world_content">
-    <module type="layouts" template="skin-9"/>
+<div>
 
-    <div class="container safe-mode nodrop">
+    <div class="edit" rel="content" field="shop-before-content">
+        <module type="layouts" template="skin-9"/>
+    </div>
+
+    <div class="container">
         <div class="row">
             <div class="col-xl-12 mx-auto">
                 <div class="row justify-content-between">
                     <div class="<?php if ($shop_sidebar == 'true'): ?>col-sm-12 col-md-8 col-lg-9 col-xl-9<?php else: ?>col-12<?php endif; ?>">
-                        <module type="shop/products" <?php if ($shop_sidebar == 'false'): ?>template="skin-1"<?php endif; ?>/>
+                        <div class="edit" rel="content" field="shop-content">
+                            <module type="shop/products" <?php if ($shop_sidebar == 'false'): ?>template="skin-1"<?php endif; ?>/>
+                        </div>
                     </div>
 
                     <?php if ($shop_sidebar == 'true'): ?>
                         <div class="col-sm-12 col-md-4 col-lg-3 col-xl-2">
-                            <?php include(__DIR__ . DS .'shop_sidebar.php'); ?>
+                            <div class="col-sm-12 col-md-4 col-lg-3 col-xl-2">
+                                <div class="edit" rel="content" field="shop-sidebar">
+                                    <?php include(__DIR__ . DS .'shop_sidebar.php'); ?>
+                                </div>
+                            </div>
                         </div>
                     <?php endif; ?>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="edit" rel="content" field="shop-after-content">
+        <p class="element"></p>
     </div>
 </div>
 

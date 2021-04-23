07b0c4a865008c74522107f50db4acd4ef0206f8
Nikolay <rachkuw@gmail.com>
2021-04-23 15:56:27 +0300

    update

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <?php include template_dir() . "header.php"; ?>
 
 <div class="edit main-content" rel="content" field="new-world_content">
-    <module type="layouts" template="home-sliders/skin-1"/>
+    <module type="layouts" template="home-banners/skin-1"/>
     <module type="layouts" template="skin-2"/>
     <module type="layouts" template="features/skin-1"/>
     <module type="layouts" template="features/skin-6"/>
--- modules/layouts/templates/home-banners/skin-1.php
+++ modules/layouts/templates/home-banners/skin-1.php
@@ -25,7 +25,7 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
 
 <!--.home-slider .slide .img-holder.left-->
 
-<section class="section-2 js-header-transparent <?php print $layout_classes; ?> edit safe-mode nodrop" data-overlay="1" field="layout-home-banners-skin-1-<?php print $params['id'] ?>" rel="module">
+<section class="section-2 js-header-transparent <?php print $layout_classes; ?> edit" data-overlay="1" field="layout-home-banners-skin-1-<?php print $params['id'] ?>" rel="module">
     <div class="background-image-holder d-flex w-100 justify-content-center" style="background-image: url('<?php print template_url(); ?>assets/img/slide-1.jpg');">
         <div class="container d-flex justify-content-center align-items-center">
             <div class="w-100 text-left info-holder allow-drop" <?php if(is_live_edit()): ?>no-<?php endif;?>data-aos="fade-down">
@@ -34,14 +34,11 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
                 <p class="lead">Drag and drop open-source website builder<br/>and CMS of new generation.</p>
                 <br/>
                 <br/>
-                <div class="element d-inline-block cloneable">
-                    <module type="btn" template="bootstrap" button_style="btn-primary" button_size="btn-lg" text="Call to action"/>
+
+                <div class="element">
+                    <a href="" class="btn btn-primary">Call to action</a>
                 </div>
             </div>
-        </div>
-
-        <div class="socials-holder">
-            <module type="social_links" template="skin-1"/>
         </div>
     </div>
 </section>

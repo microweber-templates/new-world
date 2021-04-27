64fa15fe421d9399b0857ac094d74b71ac70d6e0
Nikolay <rachkuw@gmail.com>
2021-04-27 17:29:29 +0300

    update

--- modules/layouts/templates/blockquotes/skin-3.php
+++ modules/layouts/templates/blockquotes/skin-3.php
@@ -23,8 +23,8 @@ if (!$classes['padding_bottom']) {
 $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom'] . ' ';
 ?>
 
-<div data-parallax="" data-overlay="5" data-overlay-black="" class="edit safe-mode nodrop" field="layout-blockquotes-skin-3-<?php print $params['id'] ?>" rel="module">
-    <section class="section-15 d-flex background-image-holder" style="background-image: url();">
+<div class="edit safe-mode nodrop" field="layout-blockquotes-skin-3-<?php print $params['id'] ?>" rel="module">
+    <section class="section-15 d-flex">
         <div class="container align-self-center allow-drop">
             <module type="testimonials" template="default"/>
         </div>

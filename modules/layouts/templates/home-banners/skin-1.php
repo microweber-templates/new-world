caa9347d1d5589fcd704cc788a8b1ca4509f64c9
Nikolay <rachkuw@gmail.com>
2021-04-29 10:40:18 +0300

    update

--- modules/layouts/templates/home-banners/skin-1.php
+++ modules/layouts/templates/home-banners/skin-1.php
@@ -25,7 +25,7 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
 
 <!--.home-slider .slide .img-holder.left-->
 
-<section class="section-2 js-header-transparent <?php print $layout_classes; ?> edit" data-overlay="1" field="layout-home-banners-skin-1-<?php print $params['id'] ?>" rel="module">
+<section class="section-2 js-header-transparent pt-0 pb-0 <?php print $layout_classes; ?> edit" data-overlay="1" field="layout-home-banners-skin-1-<?php print $params['id'] ?>" rel="module">
     <div class="background-image-holder d-flex w-100 justify-content-center" style="background-image: url('<?php print template_url(); ?>assets/img/slide-1.jpg');">
         <div class="container d-flex justify-content-center align-items-center">
             <div class="w-100 text-left info-holder allow-drop" <?php if(is_live_edit()): ?>no-<?php endif;?>data-aos="fade-down">

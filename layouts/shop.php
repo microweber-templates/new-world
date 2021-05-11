2d15f02a2714606119006ec93fdc1fb26ba8ec65
Nikolay <rachkuw@gmail.com>
2021-05-11 10:57:16 +0300

    update

--- layouts/shop.php
+++ layouts/shop.php
@@ -19,36 +19,32 @@ if (!isset($shop_sidebar)) {
 }
 ?>
 
-<div>
+<div class="container">
+   <div class="row">
+       <div class="edit" rel="content" field="shop-before-content">
+           <module type="layouts" template="skin-9"/>
+       </div>
 
-    <div class="edit" rel="content" field="shop-before-content">
-        <module type="layouts" template="skin-9"/>
-    </div>
+       <div class="row justify-content-between">
+           <div class="<?php if ($shop_sidebar == 'true'): ?>col-12 col-md-8 col-lg-9 col-xl-9<?php else: ?>col-12<?php endif; ?>">
+               <div class="edit" rel="content" field="shop-content">
+                   <module type="shop/products" <?php if ($shop_sidebar == 'false'): ?>template="skin-1"<?php endif; ?>/>
+               </div>
+           </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-xl-12 mx-auto">
-                <div class="row justify-content-between">
-                    <div class="<?php if ($shop_sidebar == 'true'): ?>col-sm-12 col-md-8 col-lg-9 col-xl-9<?php else: ?>col-12<?php endif; ?>">
-                        <div class="edit" rel="content" field="shop-content">
-                            <module type="shop/products" <?php if ($shop_sidebar == 'false'): ?>template="skin-1"<?php endif; ?>/>
-                        </div>
-                    </div>
+           <?php if ($shop_sidebar == 'true'): ?>
+               <div class="col-12 col-md-4 col-lg-3 col-xl-2">
+                   <div class="edit" rel="content" field="shop-sidebar">
+                       <?php include(__DIR__ . DS .'shop_sidebar.php'); ?>
+                   </div>
+               </div>
+           <?php endif; ?>
+       </div>
 
-                    <?php if ($shop_sidebar == 'true'): ?>
-                        <div class="col-sm-12 col-md-4 col-lg-3 col-xl-2">
-                            <div class="edit" rel="content" field="shop-sidebar">
-                                <?php include(__DIR__ . DS .'shop_sidebar.php'); ?>
-                            </div>
-                        </div>
-                    <?php endif; ?>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="edit" rel="content" field="shop-after-content">
-        <p class="element"></p>
-    </div>
+       <div class="edit" rel="content" field="shop-after-content">
+           <p class="element"></p>
+       </div>
+   </div>
 </div>
 
 <?php include template_dir() . "footer.php"; ?>

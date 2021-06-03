9b001a8280a9f0b538fadbc3d40f2b9bc1a7a322
Bozhidar Slaveykov <bobi@microweber.com>
2021-06-03 11:16:11 +0300

    update

--- layouts/shop.php
+++ layouts/shop.php
@@ -13,32 +13,16 @@ position: 2
 
 <?php include template_dir() . "header.php"; ?>
 
-<?php
-if (!isset($shop_sidebar)) {
-    $shop_sidebar = false;
-}
-?>
 
 <div class="container">
    <div class="row">
+
        <div class="edit" rel="content" field="shop-before-content">
            <module type="layouts" template="skin-9"/>
        </div>
 
        <div class="row justify-content-between">
-           <div class="<?php if ($shop_sidebar == 'true'): ?>col-12 col-md-8 col-lg-9 col-xl-9<?php else: ?>col-12<?php endif; ?>">
-               <div class="edit" rel="content" field="shop-content">
-                   <module type="shop/products" <?php if ($shop_sidebar == 'false'): ?>template="skin-1"<?php endif; ?>/>
-               </div>
-           </div>
-
-           <?php if ($shop_sidebar == 'true'): ?>
-               <div class="col-12 col-md-4 col-lg-3 col-xl-2">
-                   <div class="edit" rel="content" field="shop-sidebar">
-                       <?php include(__DIR__ . DS .'shop_sidebar.php'); ?>
-                   </div>
-               </div>
-           <?php endif; ?>
+           <module type="shop" />
        </div>
 
        <div class="edit" rel="content" field="shop-after-content">

7e0fac10dae39be94a3dac4921ff8e356141db49
Nikolay <rachkuw@gmail.com>
2021-06-17 11:18:21 +0300

    update

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $config['author'] = "Microweber";
 $config['version'] = 2.7;
 $config['url'] = "http://microweber.com";
 $config['standalone_module_skins'] = true;
-$config['framework'] = "bootstrap4";
+$config['framework'] = "bootstrap5";
 
 $config['is_default'] = 1; //if you set this parameter the template will be selected on the install screen
 

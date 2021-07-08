fc785c41d660c596ca69b14c115509f22ebc288b
Nikolay <rachkuw@gmail.com>
2021-07-08 13:37:30 +0300

    update

--- modules/layouts/templates/info-blocks/skin-1.php
+++ modules/layouts/templates/info-blocks/skin-1.php
@@ -28,7 +28,7 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
         <div class="row">
             <div class="col-xl-10 mx-auto">
                 <div class="row">
-                    <div class="order-2 order-xl-1 text-center text-sm-center text-lg-start col-md-10 mx-auto col-lg-6 m-b-20 d-flex flex-column justify-content-center align-items-center">
+                    <div class="order-2 order-xl-1 text-center text-sm-center text-lg-start text-lg-left  col-md-10 mx-auto col-lg-6 m-b-20 d-flex flex-column justify-content-center align-items-center">
                         <div class="allow-drop">
                             <h2 class="m-b-30">Create a beautiful website easily with Microweber</h2>
                             <p>Microweber is an open-source content management system and website builder. It is based on the PHP programming language and the Laravel 5 web application framework, using drag and drop and allowing users to quickly create content, while scheduling and managing multiple
@@ -40,7 +40,7 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
                         </div>
                     </div>
 
-                    <div class="order-1 order-xl-2 text-start text-sm-center text-xl-start col-lg-6 m-b-20">
+                    <div class="order-1 order-xl-2 text-start text-left text-sm-center text-xl-start col-lg-6 m-b-20">
                         <img src="<?php print template_url(); ?>assets/img/img-3.png" alt=""/>
                     </div>
                 </div>

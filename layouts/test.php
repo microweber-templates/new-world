18d15b6e5761cff85dc26f85dbd4fcf1dce29ecb
Nikolay <rachkuw@gmail.com>
2021-08-23 12:08:06 +0300

    update

--- layouts/test.php
+++ layouts/test.php
@@ -20,8 +20,6 @@
     <module type="layouts" template="posts/skin-3"/>
     <module type="layouts" template="posts/skin-4"/>
     <module type="layouts" template="posts/skin-5"/>
-
-
 </div>
 
 <?php include template_dir() . "footer.php"; ?>

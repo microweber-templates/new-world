daf5af9c656fd3865f60bc01d488a11783023697
raikov <raikov@ymail.com>
2021-09-07 16:13:20 +0300

    fa

--- header.php
+++ header.php
@@ -18,8 +18,6 @@
     <meta property="og:site_name" content="{og_site_name}"/>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" crossorigin="anonymous"/>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.0/css/v4-shims.css"/>
     <link rel="stylesheet" href="<?php print template_url(); ?>assets/fonts/fontawesome-free-5.15.4-web/css/all.css"/>
     <script>
         mw.require('icon_selector.js');
@@ -48,7 +46,7 @@
 
     <?php print get_template_stylesheet(); ?>
 
- 
+
 
     <?php include('template_settings.php'); ?>
 </head>

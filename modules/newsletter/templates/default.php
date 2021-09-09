c859f666629cbf78317186a613cfdb44d8e622b0
Nikolay <rachkuw@gmail.com>
2021-09-09 14:16:06 +0300

    update

--- modules/newsletter/templates/default.php
+++ modules/newsletter/templates/default.php
@@ -15,13 +15,13 @@ description: Default
     <?php print csrf_form(); ?>
 
     <div class="row m-0">
-        <div class="col-sm-9 p-0">
+        <div class="col-10 p-0">
             <div class="form-group">
                 <input type="email" name="email" id="email" class="form-control input-lg" placeholder="Email"/>
             </div>
         </div>
 
-        <div class="col-sm-3 p-0">
+        <div class="col-2 p-0">
             <div class="form-group">
                 <button class="btn btn-primary btn-block text-center" type="submit" style="padding: 7px 12px !important;"><i class="fas fa-arrow-right m-0 float-none"></i></button>
             </div>

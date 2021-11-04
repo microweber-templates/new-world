f89a7fa1190642c99cf5740ecc9dd2112e98d898
Nikolay <rachkuw@gmail.com>
2021-11-04 15:03:45 +0200

    update

--- modules/menu/templates/navbar.php
+++ modules/menu/templates/navbar.php
@@ -22,7 +22,7 @@ $menu_filter['a_class'] = '';
 $menu_filter['li_submenu_class'] = 'dropdown';
 $menu_filter['li_submenu_a_class'] = 'dropdown-has-child';
 
-$menu_filter['link'] = '<a itemprop="url" data-item-id="{id}" class="menu_element_link {active_class} {exteded_classes} {nest_level} {a_class}"  href="{url}"><span>{title}</span></a>';
+$menu_filter['link'] = '<a itemprop="url" data-item-id="{id}" class="menu_element_link {active_class} {exteded_classes} {nest_level} {a_class}" {target_attribute} href="{url}"><span>{title}</span></a>';
 $menu_filter['li_submenu_a_link'] = '<a itemprop="url"  data-item-id="{id}" href="{url}" class="menu_element_link {active_class} {exteded_classes} {nest_level} {li_submenu_a_class}" aria-haspopup="true" aria-expanded="false"><span class="name">{title}</span> <span class="caret"></span></a>';
 
 $mt = menu_tree($menu_filter);
--- modules/menu/templates/simple.php
+++ modules/menu/templates/simple.php
@@ -17,7 +17,7 @@ $menu_filter['ul_class'] = '';
 $menu_filter['ul_class_deep'] = '';
 $menu_filter['li_class'] = '';
 $menu_filter['a_class'] = '';
-$menu_filter['link'] = '<a itemprop="url" data-item-id="{id}" class="menu_element_link {active_class} {exteded_classes} {nest_level} {a_class}"  href="{url}"><span>{title}</span></a>';
+$menu_filter['link'] = '<a itemprop="url" data-item-id="{id}" class="menu_element_link {active_class} {exteded_classes} {nest_level} {a_class}" {target_attribute} href="{url}"><span>{title}</span></a>';
 
 $mt = menu_tree($menu_filter);
 

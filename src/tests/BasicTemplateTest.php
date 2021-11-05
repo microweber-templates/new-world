98743cf610f8b2629cdb736eab52538b8a140158
Bozhidar Slaveykov <b.slaveykov@radioactive.bg>
2021-11-05 18:16:45 +0200

    Create BasicTemplateTest.php

---
+++ src/tests/BasicTemplateTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace MicroweberPackages\Templates\new-world\tests;
+
+use MicroweberPackages\Core\tests\TestCase;
+
+class BasicTemplateTest extends TestCase
+{
+    public function testHomepage()
+    {
+        $this->assertTrue(true);
+    }
+}

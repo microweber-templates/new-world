6a891c92dd7e8db11b1e6531c3a462f1e1e9766a
Alex <raikov@ymail.com>
2022-02-07 18:07:38 +0200

    empty

--- modules/layouts/templates/skin-1.php
+++ modules/layouts/templates/skin-1.php
@@ -30,7 +30,7 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
                 <div class="mw-row">
                     <div class="mw-col" style="width:100%">
                         <div class="mw-col-container">
-                            <div class="mw-empty"></div>
+                            <div class="mw-empty-element"></div>
                         </div>
                     </div>
                 </div>

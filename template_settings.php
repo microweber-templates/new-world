807c47e921db4723a0a4fadeeabec1ff667e7472
Peter Ivanov <peter@microweber.com>
2022-06-08 22:38:04 +0300

    update

--- template_settings.php
+++ template_settings.php
@@ -2,4 +2,9 @@
 
 <?php if (isset($params['data-parent-module-id']) AND $params['data-parent-module-id'] == 'template_settings_admin'): ?>
     <module type="editor/template_settings"/>
+
+<hr>
+
+    <module type="editor/stylesheet_compiler"/>
+
 <?php endif; ?>
